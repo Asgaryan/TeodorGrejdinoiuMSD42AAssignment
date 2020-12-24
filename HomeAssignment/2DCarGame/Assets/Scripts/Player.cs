d73e767e4bbc8f7ec1e32a92d61be3115eca28b0
Asgaryan <73109743+Asgaryan@users.noreply.github.com>
2020-12-24 23:31:41 +0100

    Created Project, Imported First Assets, Made Prefabs, Camera Boundary and Aspect Ratio, and Player Movement

---
+++ HomeAssignment/2DCarGame/Assets/Scripts/Player.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Serialized Fields
+    [SerializeField] float moveSpeed = 10f;
+
+    // Variables for border around camera
+    float xMin, xMax, yMin, yMax;
+    float padding = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetUpMoveBoundaries();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void SetUpMoveBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
+    }
+
+    private void Move()
+    {
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        var newXPos = transform.position.x + deltaX;
+        newXPos = Mathf.Clamp(newXPos, xMin, xMax);
+
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        var newYPos = transform.position.y + deltaY;
+        newYPos = Mathf.Clamp(newYPos, yMin, yMax);
+
+        transform.position = new Vector2(newXPos, newYPos);
+    }
+}
